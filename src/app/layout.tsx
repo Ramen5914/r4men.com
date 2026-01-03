3a546724229f70a1bd6462572b86f81f6f4d82ae
Omar Rahman <omar@r4men.com>
2026-01-03 06:52:33 -0800

    Add CardNav component and update layout with new font and structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,45 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import type { Metadata } from 'next';
+import { Doto, Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { GoogleAnalytics } from '@next/third-parties/google';
+
+const dotoSans = Doto({
+  variable: '--font-doto',
+  subsets: ['latin'],
+  weight: ['500'],
+});
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-	title: "Ramen",
-	description: "r4men.com",
+  title: 'Ramen',
+  description: 'r4men.com',
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<head>
-				<link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
-			</head>
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
-			<GoogleAnalytics gaId="G-9D5KGL0BKR" />
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
+      </head>
+      <body
+        className={`${dotoSans.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+      <GoogleAnalytics gaId="G-9D5KGL0BKR" />
+    </html>
+  );
 }
--- src/app/mods/page.tsx
+++ src/app/mods/page.tsx
@@ -1,17 +1,15 @@
-import { Metadata } from "next";
-import Image from "next/image";
+import { Metadata } from 'next';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
-	title: "Mods",
-	description: "Ramen's Mods",
+  title: 'Mods',
+  description: "Ramen's Mods",
 };
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Mods
-      </main>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">Mods</main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
