184956b8469b37ec43fcf4dcef424aa3d04f60bc
Omar Rahman <omar@r4men.com>
2026-08-12 05:49:21 -0700

    Organize imports

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,8 +1,8 @@
 import { loader } from "fumadocs-core/source";
 import { lucideIconsPlugin } from "fumadocs-core/source/lucide-icons";
-import { docsContentRoute, docsImageRoute, docsRoute } from "./shared";
-import { defineDocs } from "fumadocs-mdx/macro";
 import { metaSchema, pageSchema } from "fumadocs-core/source/schema";
+import { defineDocs } from "fumadocs-mdx/macro";
+import { docsContentRoute, docsImageRoute, docsRoute } from "./shared";
 
 const docs = defineDocs({
   dir: "content/docs",
