90915014032094f805cb26a9ed5be93aa9c7ea02
Omar Rahman <omar@r4men.com>
2026-08-15 23:39:21 -0700

    Testing mod cards

--- src/app/(home)/mods/page.tsx
+++ src/app/(home)/mods/page.tsx
@@ -1,8 +1,16 @@
 import Link from "next/link";
 
+import "./styles.css";
+
 export default function HomePage() {
   return (
     <div className="flex flex-col justify-center text-center flex-1">
+      <div className="grid grid-cols-2">
+        <div className="mod-card">1</div>
+        <div className="mod-card">2</div>
+        <div className="mod-card">3</div>
+        <div className="mod-card">4</div>
+      </div>
       <h1 className="text-2xl font-bold mb-4">
         Hello World (from /mods this time)
       </h1>
