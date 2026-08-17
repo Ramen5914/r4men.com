b6aacf177a1dfd0a8217e0a3695eabbb686be611
Omar Rahman <omar@r4men.com>
2026-08-16 23:22:14 -0700

    Catpuccin test

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,6 +2,8 @@ import { HomeLayout } from "fumadocs-ui/layouts/home";
 import { BookIcon } from "lucide-react";
 import { baseOptions } from "@/lib/layout.shared";
 
+// import "./mods.css";
+
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <HomeLayout
--- src/components/ModCard.tsx
+++ src/components/ModCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function ModCard({
   name,
@@ -10,25 +11,27 @@ export default function ModCard({
   const iconSize = 48;
 
   return (
-    <div
-      className={
-        "min-h-32 border-gray-850 border-2 rounded-[40px] flex-row flex p-4"
-      }
-    >
-      <div>
-        <Image
-          src={iconLink}
-          alt={"Mod icon"}
-          width={iconSize}
-          height={iconSize}
-          className="rounded-full"
-          loading="eager"
-        />
+    <Link href="/mods/game-knight">
+      <div
+        className={
+          "min-h-32 border-gray-850 border-2 rounded-[40px] flex-row flex p-4"
+        }
+      >
+        <div>
+          <Image
+            src={iconLink}
+            alt={"Mod icon"}
+            width={iconSize}
+            height={iconSize}
+            className="rounded-full"
+            loading="eager"
+          />
+        </div>
+        <div className="flex flex-col grow">
+          <span>{name}</span>
+          <span>{name}</span>
+        </div>
       </div>
-      <div className="flex flex-col grow">
-        <span>{name}</span>
-        <span>{name}</span>
-      </div>
-    </div>
+    </Link>
   );
 }
