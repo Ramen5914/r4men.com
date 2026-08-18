f540539190b5ffc90bbb0de7f1586592fc12daf6
Omar Rahman <omar@r4men.com>
2026-08-18 00:28:11 -0700

    stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,34 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { Provider } from "@/components/provider";
+import type {Metadata, Viewport} from "next";
+import {Inter} from "next/font/google";
+import {Provider} from "@/components/provider";
 
 import "./global.css";
 import "fumadocs-twoslash/twoslash.css";
 import "katex/dist/katex.css";
 
 const inter = Inter({
-  subsets: ["latin"],
+    subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "r4men.com",
-  metadataBase: new URL("https://r4men.com"),
-  alternates: {
-    canonical: "/",
-  },
+export const viewport: Viewport = {
+    width: "device-width",
+    initialScale: 1,
 };
 
-export default function Layout({ children }: LayoutProps<"/">) {
-  return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
+export const metadata: Metadata = {
+    title: "r4men.com",
+    metadataBase: new URL("https://r4men.com"),
+    alternates: {
+        canonical: "/",
+    },
+};
+
+export default function Layout({children}: LayoutProps<"/">) {
+    return (
+        <html lang="en" className={inter.className} suppressHydrationWarning>
+        <body className="flex flex-col min-h-screen">
         <Provider>{children}</Provider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
