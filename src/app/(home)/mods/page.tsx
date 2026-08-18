371f17be458fb72164503ffec6fd1de148c2bc67
Omar Rahman <omar@r4men.com>
2026-08-18 00:21:42 -0700

    Fixed mobile (?)

--- src/app/(home)/mods/page.tsx
+++ src/app/(home)/mods/page.tsx
@@ -1,19 +1,19 @@
 import ModCard from "@/components/ModCard";
 
 export default function HomePage() {
-  return (
-    <div className="flex flex-row text-center justify-center p-4">
-      <div className="flex flex-col justify-center text-center flex-1 gap-4 max-w-7xl">
-        <h1 className="">Modlist:</h1>
-        <div className="grid grid-cols-2 gap-4">
-          <ModCard name="Game Knight" location="game-knight" />
-          <ModCard
-            name="Create: Cobblemon Manufactory"
-            location="cobblemon-manufactory"
-          />
-          <ModCard name="Ramen's Additions" location="ramens-additions" />
+    return (
+        <div className="flex flex-row text-center justify-center p-4">
+            <div className="flex flex-col justify-center text-center flex-1 gap-4 max-w-7xl">
+                <h1 className="">Modlist:</h1>
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                    <ModCard name="Game Knight" location="game-knight"/>
+                    <ModCard
+                        name="Create: Cobblemon Manufactory"
+                        location="cobblemon-manufactory"
+                    />
+                    <ModCard name="Ramen's Additions" location="ramens-additions"/>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
