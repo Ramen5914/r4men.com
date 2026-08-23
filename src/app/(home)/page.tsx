06fb1f661ee08ba8298817d93b06aca3b20a042d
Omar Rahman <omar@r4men.com>
2026-08-23 05:00:10 -0700

    Changed front page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,14 +3,9 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 font-bold text-2xl">Hello World</h1>
-      <p>
-        You can open{" "}
-        <Link href="/mods" className="font-medium underline">
-          /mods
-        </Link>{" "}
-        and see the documentation.
-      </p>
+      <h1 className={"mb-4 font-bold text-2xl"}>
+        I really wanted ramen.com but it was too expensive :(
+      </h1>
     </div>
   );
 }
